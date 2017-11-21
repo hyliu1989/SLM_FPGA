db3b766ad77d5480b9188a0f022d25d753ad771a
Herbert at Laura's Laptop <hyhliu1989@gmail.com>
2017-11-21 12:03:54 -0800

    fix a delay cycle bug of Avalon protocol in FIFO reading

--- v/jtag_uart_decode.v
+++ v/jtag_uart_decode.v
@@ -139,7 +139,6 @@ module uart_avalon_extraction(
     // data to parse
     output [7:0]  oDATA_TO_PARSE,
     output        oDATA_TO_PARSE_VALID
-
 );
 /*
                             ____      ____      ____      ____      ____
@@ -148,35 +147,39 @@ clock                 _____|    |____|    |____|    |____|    |____|    |____
 req                   _____|
                             _________ 
 read_wait             _____|         |______________________________
-                      _________________________ _________ _________ _________
-data_from_uart        _________________________|____1____|____2____|____3____|   assuming Avalon latency is 1 cycle of deasserted read_wait.
-                                                ______________________________
-data_from_uart_ready  _________________________|
-                      ___________________________________ _________ _________
-data_to_parse         ___________________________________|____1____|____2____|
-                                                          _________ _________
-data_to_parse_valid   ___________________________________|____X____|____X____|   depends on the valid bit of data_from_uart
+                      _______________ _________ _________ _________
+data_from_uart        _______________|____1____|____2____|____3____|   Avalon latency is 0 cycle after a deasserted read_wait.
+                                      ______________________________
+data_from_uart_ready  _______________|
+                      _________________________ _________ _________
+data_to_parse         _________________________|____1____|____2____|
+                                                _________ _________
+data_to_parse_valid   _________________________|____X____|____X____|   depends on the valid bit of data_from_uart
 
 */
 parameter ST_UART_IDLE =         2'b00;
 parameter ST_UART_READ =         2'b01;
 reg [1:0]  states_uart;
-reg        data_from_uart_ready, data_from_uart_ready_next;
+wire       data_from_uart_ready;
+wire [7:0] data_from_uart;
+wire       data_from_uart15;
 reg [7:0]  data_to_parse;
 reg        data_to_parse_valid;
+
+assign data_from_uart_ready = (states_uart == ST_UART_READ) && (!iJTAG_SLAVE_WAIT);
+assign data_from_uart = iJTAG_SLAVE_RDDATA[7:0];
+assign data_from_uart15 = iJTAG_SLAVE_RDDATA[15];
 
 always @ (posedge iCLK or posedge iRST) begin
     if(iRST) begin
         states_uart <= ST_UART_IDLE;
-        data_from_uart_ready <= 1'b0;
         data_to_parse_valid <= 1'b0;
         data_to_parse <= 8'd0;
     end
     else begin
         states_uart <= ST_UART_READ;
-        data_from_uart_ready <= (states_uart == ST_UART_READ) && (!iJTAG_SLAVE_WAIT);
-        data_to_parse_valid <= (data_from_uart_ready && iJTAG_SLAVE_RDDATA[15]);
-        data_to_parse <= iJTAG_SLAVE_RDDATA[7:0];
+        data_to_parse_valid <= (data_from_uart_ready && data_from_uart15);
+        data_to_parse <= data_from_uart;
     end
 end
 
