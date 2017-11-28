dd57f849d21ba9fbf2e99e2a75433c70248f8e9f
Herbert at Laura's Laptop <hyhliu1989@gmail.com>
2017-11-28 03:25:19 -0800

    dissect the jtag_uart_decode module

---
+++ v/jtag_uart/jtag_decoder_param.h
@@ -0,0 +1,56 @@
+// Commands after the Escape character
+parameter ESCAPECMD_RETURN_TO_IDLE             = 8'h00;
+parameter ESCAPECMD_ACKNOWLEDGE                = 8'h01;
+parameter ESCAPECMD_ESCAPE_CHAR                = 8'hFE;
+
+// Commands used in PC
+parameter HOSTCMD_ESCAPE                    = 8'hFE;
+
+parameter HOSTCMD_SEND_IMAGES               = 8'b10??_????;  // Update memory
+parameter HOSTCMD_SEND_SINGLE               = 8'b01??_????;  // Update memory of one image
+parameter HOSTCMD_UPDATE_OFFSET_H           = 8'h01;  // Update offsets
+parameter HOSTCMD_UPDATE_OFFSET_V           = 8'h02;  // Update offsets
+parameter HOSTCMD_UPDATE_CYC_DISPLAY        = 8'h03;  // Update number of displaying cycles for each image
+parameter HOSTCMD_TRIGGER_SEQUENCING        = 8'h04;  // Triggering the sequencing
+parameter HOSTCMD_UPDATE_GALVO_X            = 8'h05;  // Update Galvo informations
+parameter HOSTCMD_UPDATE_GALVO_Y            = 8'h06;  // Update Galvo informations
+
+
+// System states
+parameter ST_IDLE                              = 7'h0_0;
+parameter INSTRUCTION_ACK                      = 7'h1_0;  // not a state but just an acknowledge instruction
+parameter ST_LISTEN_TO_INTERRUPT               = 7'h2_0;  // waiting for instruction
+parameter ST_WAIT_ACK                          = 7'h6_0;
+parameter ST_ERROR                             = 7'h7_0;
+
+// Update memory
+parameter ST_UPDATE_RAM_get_num_of_frames      = 7'h0_1;
+parameter ST_UPDATE_RAM_trigger                = 7'h1_1;
+parameter ST_UPDATE_RAM_wait_first_data        = 7'h2_1;
+parameter ST_UPDATE_RAM_wait_data              = 7'h3_1;
+
+// Update memory of one image
+parameter ST_UPDATE_RAM_SINGLE_get_frame_id    = 7'h0_6;
+parameter ST_UPDATE_RAM_SINGLE_trigger         = 7'h1_6;
+parameter ST_UPDATE_RAM_SINGLE_wait_first      = 7'h2_6;
+parameter ST_UPDATE_RAM_SINGLE_wait_data       = 7'h3_6;
+
+// Update offsets
+parameter ST_UPDATE_OFFSET_horizontal          = 7'h0_2;
+parameter ST_UPDATE_OFFSET_vertical            = 7'h1_2;
+parameter ST_UPDATE_OFFSET_get_number          = 7'h2_2;
+parameter ST_UPDATE_OFFSET_get_sign            = 7'h3_2;
+
+// Update number of displaying cycles for each image
+parameter ST_UPDATE_DISPLAY_CYC_get_num        = 7'h0_3;
+parameter ST_UPDATE_DISPLAY_CYC_get_num_1      = 7'h1_3;
+
+// Triggering the sequencing
+parameter ST_START_SEQUENCE_trigger            = 7'h0_4;
+
+// Update Galvo informations
+parameter ST_UPDATE_GALVO_for_x                = 7'h0_5;
+parameter ST_UPDATE_GALVO_for_y                = 7'h1_5;
+parameter ST_UPDATE_GALVO_value_0              = 7'h2_5;
+parameter ST_UPDATE_GALVO_value_1              = 7'h3_5;
+parameter ST_UPDATE_GALVO_value_2              = 7'h4_5;
--- v/jtag_uart/jtag_uart_decode.v
+++ v/jtag_uart/jtag_uart_decode.v
@@ -5,7 +5,7 @@ module jtag_uart_decode(
     // jtag uart signals
     output        oJTAG_SLAVE_ADDR,
     output        oJTAG_SLAVE_RDREQ,
-    input [31:0]  iJTAG_SLAVE_RDDATA,
+    input  [31:0] iJTAG_SLAVE_RDDATA,
     output        oJTAG_SLAVE_WRREQ,
     output [31:0] oJTAG_SLAVE_WRDATA,
     input         iJTAG_SLAVE_WAIT,
@@ -35,237 +35,35 @@ module jtag_uart_decode(
     output [6:0]  oMONITORING_STATES
 );
 
-// ====================================================
-//   Communicating with JTAG-UART core
-// ====================================================
-wire [7:0]  r0_data_or_cmd;
-wire        r0_data_or_cmd_valid;
-reg  [7:0]  data_or_cmd, r1_data_or_cmd;
-reg         data_or_cmd_valid, r1_data_or_cmd_valid;
-uart_avalon_extraction uart_avalon_extraction_0(
+`include "jtag_decoder_param.h"
+
+
+wire        is_idle_to_take_command;
+reg         new_instr_ack, new_data_ack;
+wire        is_there_new_instruct, is_there_new_data;
+wire [6:0]  state_instuction;
+wire [7:0]  data;
+
+decode_raw_jtag_uart raw_0(
     .iCLK(iCLK),
     .iRST(iRST),
+
+    // jtag uart signals
     .oJTAG_SLAVE_ADDR(oJTAG_SLAVE_ADDR),
     .oJTAG_SLAVE_RDREQ(oJTAG_SLAVE_RDREQ),
     .iJTAG_SLAVE_RDDATA(iJTAG_SLAVE_RDDATA),
     .oJTAG_SLAVE_WRREQ(oJTAG_SLAVE_WRREQ),
     .oJTAG_SLAVE_WRDATA(oJTAG_SLAVE_WRDATA),
     .iJTAG_SLAVE_WAIT(iJTAG_SLAVE_WAIT),
-    .oDATA_TO_PARSE(r0_data_or_cmd),
-    .oDATA_TO_PARSE_VALID(r0_data_or_cmd_valid)
+
+    .iDECODER_IDLE_TO_TAKE_COMMAND(is_idle_to_take_command),
+    .iDECODER_ACK_INSTR(new_instr_ack),
+    .iDECODER_ACK_DATA(new_data_ack),
+    .oIS_THERE_NEW_INSTRUCTION(is_there_new_instruct),
+    .oIS_THERE_NEW_DATA(is_there_new_data),
+    .oSTATE_INSTRUCTION(state_instuction),
+    .oDATA(data)
 );
-always @ (posedge iCLK) begin
-    r1_data_or_cmd <= r0_data_or_cmd;
-    r1_data_or_cmd_valid <= r0_data_or_cmd_valid;
-    data_or_cmd <= r1_data_or_cmd;
-    data_or_cmd_valid <= r1_data_or_cmd_valid;
-end
-
-
-reg [6:0]   states, states_next, previous_states;
-reg         new_instr_ack, new_data_ack;
-parameter HOSTCMD_ESCAPE                    = 8'hFE;
-parameter ESCAPECMD_RETURN_TO_IDLE             = 8'h00;
-parameter ESCAPECMD_ACKNOWLEDGE                = 8'h01;
-parameter ESCAPECMD_ESCAPE_CHAR                = 8'hFE;
-
-parameter ST_IDLE                              = 7'h0_0;
-parameter INSTRUCTION_ACK                      = 7'h1_0;  // not a state but just an acknowledge instruction
-parameter ST_LISTEN_TO_INTERRUPT               = 7'h2_0;
-parameter ST_WAIT_ACK                          = 7'h6_0;
-parameter ST_ERROR                             = 7'h7_0;
-
-parameter HOSTCMD_SEND_IMAGES               = 8'b10??_????;
-parameter ST_UPDATE_RAM_get_num_of_frames      = 7'h0_1;
-
-parameter HOSTCMD_UPDATE_OFFSET_H           = 8'h01;
-parameter HOSTCMD_UPDATE_OFFSET_V           = 8'h02;
-parameter ST_UPDATE_OFFSET_horizontal          = 7'h0_2;
-parameter ST_UPDATE_OFFSET_vertical            = 7'h1_2;
-
-parameter HOSTCMD_UPDATE_CYC_DISPLAY        = 8'h03;
-parameter ST_UPDATE_DISPLAY_CYC_get_num        = 7'h0_3;
-
-parameter HOSTCMD_TRIGGER_SEQUENCING        = 8'h04;
-parameter ST_START_SEQUENCE_trigger            = 7'h0_4;
-
-parameter HOSTCMD_UPDATE_GALVO_X            = 8'h05;
-parameter HOSTCMD_UPDATE_GALVO_Y            = 8'h06;
-parameter ST_UPDATE_GALVO_for_x                = 7'h0_5;
-parameter ST_UPDATE_GALVO_for_y                = 7'h1_5;
-
-parameter HOSTCMD_SEND_SINGLE               = 8'b01??_????;
-parameter ST_UPDATE_RAM_SINGLE_get_frame_id    = 7'h0_6;
-
-
-// =============================================================
-//   Data parsing to extract state instruction and actual data
-// =============================================================
-reg         received_0xFE, received_0xFE_next;
-reg [6:0]   state_instuction, state_instuction_next;  // an instruction to states, `state_instuction` will be check as `states` progresses.
-reg         is_there_new_instruct, is_there_new_instruct_next;
-reg [7:0]   data, data_next;
-reg         is_there_new_data, is_there_new_data_next;
-
-always @ (*) begin
-    // received_0xFE_next
-    if(data_or_cmd_valid) begin
-        if(received_0xFE)
-            received_0xFE_next = 1'b0;
-        else
-            received_0xFE_next = (data_or_cmd == HOSTCMD_ESCAPE);
-    end
-    else
-        received_0xFE_next = received_0xFE;
-    
-    
-    // state_instuction_next, is_there_new_instruct_next, data_next, is_there_new_data_next
-    if(data_or_cmd_valid) begin
-        // 0xFE interruption behavior
-        if(received_0xFE) begin
-            // For instruction to the states
-            case(data_or_cmd)
-                ESCAPECMD_RETURN_TO_IDLE: begin
-                    state_instuction_next = ST_IDLE;
-                    is_there_new_instruct_next = 1'b1;
-                end
-                ESCAPECMD_ACKNOWLEDGE: begin
-                    state_instuction_next = INSTRUCTION_ACK;
-                    is_there_new_instruct_next = 1'b1;
-                end
-                ESCAPECMD_ESCAPE_CHAR: begin
-                    state_instuction_next = 7'bxxx_xxxx;
-                    is_there_new_instruct_next = 1'b0;  // no new state to move to
-                end
-                default: begin
-                    state_instuction_next = ST_ERROR;
-                    is_there_new_instruct_next = 1'b1;
-                end
-            endcase
-            
-            // For data, the only data after a 0xFE escaping
-            data_next              = (data_or_cmd == ESCAPECMD_ESCAPE_CHAR)? HOSTCMD_ESCAPE : 8'bxxxx_xxxx;
-            is_there_new_data_next = (data_or_cmd == ESCAPECMD_ESCAPE_CHAR)? 1'b1 : 1'b0;
-        end
-
-        // normal behavior
-        else begin
-            data_next = data_or_cmd;
-            // When Idle or Idle with Error, take instructions
-            if(states == ST_IDLE || states == ST_ERROR) begin
-                casez(data_or_cmd)
-                    // Kick off transferring data to SDRAM
-                    HOSTCMD_SEND_IMAGES: begin
-                        state_instuction_next = ST_UPDATE_RAM_get_num_of_frames;
-                        is_there_new_instruct_next = 1'b1;
-                        is_there_new_data_next = 1'b1;  // the data in this case is the number of frames to transfer minus 1.
-                    end
-                    
-                    // Kick off transferring single image data to the SDRAM
-                    HOSTCMD_SEND_SINGLE: begin
-                        state_instuction_next = ST_UPDATE_RAM_SINGLE_get_frame_id;
-                        is_there_new_instruct_next = 1'b1;
-                        is_there_new_data_next = 1'b1;  // the data in this case is the frames id (0~63 inclusive).
-                    end
-
-                    // Kick off updating horizontal offset
-                    HOSTCMD_UPDATE_OFFSET_H: begin
-                        state_instuction_next = ST_UPDATE_OFFSET_horizontal;
-                        is_there_new_instruct_next = 1'b1;
-                        is_there_new_data_next = 1'b0;
-                    end
-                    
-                    // Kick off updating vertical offset
-                    HOSTCMD_UPDATE_OFFSET_V: begin
-                        state_instuction_next = ST_UPDATE_OFFSET_vertical;
-                        is_there_new_instruct_next = 1'b1;
-                        is_there_new_data_next = 1'b0;
-                    end
-                    
-                    // Kick off updating displaying cycles
-                    HOSTCMD_UPDATE_CYC_DISPLAY: begin
-                        state_instuction_next = ST_UPDATE_DISPLAY_CYC_get_num;
-                        is_there_new_instruct_next = 1'b1;
-                        is_there_new_data_next = 1'b0;
-                    end
-                    
-                    // Kick off the sequencing
-                    HOSTCMD_TRIGGER_SEQUENCING: begin
-                        state_instuction_next = ST_START_SEQUENCE_trigger;
-                        is_there_new_instruct_next = 1'b1;
-                        is_there_new_data_next = 1'b0;
-                    end
-                    
-                    // Kick off updating galvo x
-                    HOSTCMD_UPDATE_GALVO_X: begin
-                        state_instuction_next = ST_UPDATE_GALVO_for_x;
-                        is_there_new_instruct_next = 1'b1;
-                        is_there_new_data_next = 1'b0;
-                    end
-                    
-                    // Kick off updating galvo y
-                    HOSTCMD_UPDATE_GALVO_Y: begin
-                        state_instuction_next = ST_UPDATE_GALVO_for_y;
-                        is_there_new_instruct_next = 1'b1;
-                        is_there_new_data_next = 1'b0;
-                    end
-
-                    // Bypass the escaping
-                    HOSTCMD_ESCAPE: begin
-                        state_instuction_next = 7'bxxx_xxxx;
-                        is_there_new_instruct_next = 1'b0;
-                        is_there_new_data_next = 1'b0;
-                    end
-
-                    // otherwise, go to the error state
-                    default: begin
-                        state_instuction_next = ST_ERROR;
-                        is_there_new_instruct_next = 1'b1;
-                        is_there_new_data_next = 1'b0;
-                    end
-                endcase
-            end
-            // When machine is in some processing, no instruction but obtain the data.
-            else begin
-                if(data_or_cmd == HOSTCMD_ESCAPE) begin
-                    state_instuction_next = 7'bxxx_xxxx;
-                    is_there_new_instruct_next = 1'b0;
-                    is_there_new_data_next = 1'b0;
-                end
-                else begin
-                    state_instuction_next = 7'bxxx_xxxx;
-                    is_there_new_instruct_next = 1'b0;
-                    is_there_new_data_next = 1'b1;
-                end
-            end
-        end
-    end
-    // when data_or_cmd is not valid, mostly hold the positions of the 4 registers
-    else begin
-        state_instuction_next = state_instuction;
-        is_there_new_instruct_next = (new_instr_ack)? 1'b0 : is_there_new_instruct;
-        
-        data_next = data;
-        is_there_new_data_next = (new_data_ack)? 1'b0 : is_there_new_data;
-    end
-end
-
-always @ (posedge iCLK or posedge iRST) begin
-    if(iRST) begin
-        received_0xFE <= 1'b0;
-        state_instuction <= 7'd0;
-        is_there_new_instruct <= 1'b0;
-        data <= 8'd0;
-        is_there_new_data <= 1'b0;
-    end
-    else begin
-        received_0xFE <= received_0xFE_next;
-        state_instuction <= state_instuction_next;
-        is_there_new_instruct <= is_there_new_instruct_next;
-        data <= data_next;
-        is_there_new_data <= is_there_new_data_next;
-    end
-end
 
 
 
@@ -275,46 +73,7 @@ end
 // new_instr_ack is assigned 1 whenever state_instuction is used.
 // new_data_ack is assigned 1 whenever state_instuction is used.
 // For state transition, check the state instruction only at the states with "_idle" appended
-
-/* These state parameters are defined above
-parameter ST_IDLE                              = 7'h0_0;
-parameter INSTRUCTION_ACK                      = 7'h1_0;  // not a state but just an acknowledge instruction
-parameter ST_LISTEN_TO_INTERRUPT               = 7'h2_0;  // waiting for instruction
-parameter ST_WAIT_ACK                          = 7'h6_0;
-parameter ST_ERROR                             = 7'h7_0;  */
-
-/* Update memory 
-parameter ST_UPDATE_RAM_get_num_of_frames      = 7'h0_1;  */
-parameter ST_UPDATE_RAM_trigger                = 7'h1_1;
-parameter ST_UPDATE_RAM_wait_first_data        = 7'h2_1;
-parameter ST_UPDATE_RAM_wait_data              = 7'h3_1;
-
-/* Update memory of one image
-parameter ST_UPDATE_RAM_SINGLE_get_frame_id    = 7'h0_6;  */
-parameter ST_UPDATE_RAM_SINGLE_trigger         = 7'h1_6;
-parameter ST_UPDATE_RAM_SINGLE_wait_first      = 7'h2_6;
-parameter ST_UPDATE_RAM_SINGLE_wait_data       = 7'h3_6;
-
-/* Update offsets
-parameter ST_UPDATE_OFFSET_horizontal          = 7'h0_2;
-parameter ST_UPDATE_OFFSET_vertical            = 7'h1_2;  */
-parameter ST_UPDATE_OFFSET_get_number          = 7'h2_2;
-parameter ST_UPDATE_OFFSET_get_sign            = 7'h3_2;
-
-/* Update number of displaying cycles for each image
-parameter ST_UPDATE_DISPLAY_CYC_get_num        = 7'h0_3;  */
-parameter ST_UPDATE_DISPLAY_CYC_get_num_1      = 7'h1_3;
-
-/* Triggering the sequencing
-parameter ST_START_SEQUENCE_trigger            = 7'h0_4;  */
-
-/* Update Galvo informations
-parameter ST_UPDATE_GALVO_for_x                = 7'h0_5;
-parameter ST_UPDATE_GALVO_for_y                = 7'h1_5;  */
-parameter ST_UPDATE_GALVO_value_0              = 7'h2_5;
-parameter ST_UPDATE_GALVO_value_1              = 7'h3_5;
-parameter ST_UPDATE_GALVO_value_2              = 7'h4_5;
-
+reg [6:0]   states, states_next, previous_states;
 
 reg [6:0]   total_frames, total_frames_next;
 reg [25:0]  counter, counter_next, r_counter;
@@ -331,6 +90,8 @@ reg [15:0]  cycles_of_display, cycles_of_display_next;
 
 reg         update_galvo_horiz, update_galvo_horiz_next;
 reg [23:0]  galvo_values_x, galvo_values_x_next, galvo_values_y, galvo_values_y_next;
+
+assign is_idle_to_take_command = (states==ST_IDLE)||(states==ST_ERROR);
 
 always @ (*) begin
     case(states)
@@ -714,78 +475,4 @@ end
 
 assign oERROR = (states == ST_ERROR);
 assign oMONITORING_STATES = states;
-endmodule
-
-
-
-
-
-
-module uart_avalon_extraction(
-    input iCLK,
-    input iRST,
-
-    // jtag uart signals
-    output        oJTAG_SLAVE_ADDR,
-    output        oJTAG_SLAVE_RDREQ,
-    input [31:0]  iJTAG_SLAVE_RDDATA,
-    output        oJTAG_SLAVE_WRREQ,
-    output [31:0] oJTAG_SLAVE_WRDATA,
-    input         iJTAG_SLAVE_WAIT,
-
-    // data to parse
-    output [7:0]  oDATA_TO_PARSE,
-    output        oDATA_TO_PARSE_VALID
-);
-/*
-                            ____      ____      ____      ____      ____
-clock                 _____|    |____|    |____|    |____|    |____|    |____
-                            _____________________________________
-req                   _____|
-                            _________ 
-read_wait             _____|         |______________________________
-                      _______________ _________ _________ _________
-data_from_uart        _______________|____1____|____2____|____3____|   Avalon latency is 0 cycle after a deasserted read_wait.
-                                      ______________________________
-data_from_uart_ready  _______________|
-                      _________________________ _________ _________
-data_to_parse         _________________________|____1____|____2____|
-                                                _________ _________
-data_to_parse_valid   _________________________|____X____|____X____|   depends on the valid bit of data_from_uart
-
-*/
-parameter ST_UART_IDLE =         2'b00;
-parameter ST_UART_READ =         2'b01;
-reg [1:0]  states_uart;
-wire       data_from_uart_ready;
-wire [7:0] data_from_uart;
-wire       data_from_uart15;
-reg [7:0]  data_to_parse;
-reg        data_to_parse_valid;
-
-assign data_from_uart_ready = (states_uart == ST_UART_READ) && (!iJTAG_SLAVE_WAIT);
-assign data_from_uart = iJTAG_SLAVE_RDDATA[7:0];
-assign data_from_uart15 = iJTAG_SLAVE_RDDATA[15];
-
-always @ (posedge iCLK or posedge iRST) begin
-    if(iRST) begin
-        states_uart <= ST_UART_IDLE;
-        data_to_parse_valid <= 1'b0;
-        data_to_parse <= 8'd0;
-    end
-    else begin
-        states_uart <= ST_UART_READ;
-        data_to_parse_valid <= (data_from_uart_ready && data_from_uart15);
-        data_to_parse <= data_from_uart;
-    end
-end
-
-assign oJTAG_SLAVE_ADDR = 1'b0;  // always look at the DATA fifo
-assign oJTAG_SLAVE_RDREQ = (states_uart == ST_UART_READ);
-assign oJTAG_SLAVE_WRREQ = 1'b0;
-assign oJTAG_SLAVE_WRDATA = 32'd0;
-
-assign oDATA_TO_PARSE = data_to_parse;
-assign oDATA_TO_PARSE_VALID = data_to_parse_valid;
-
 endmodule
